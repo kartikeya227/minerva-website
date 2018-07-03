57c3df66cdfbc9d9dc73e3126b446b4728e805af
kartikeya227 <kartikeya22798@gmail.com>
2018-07-03 16:28:52 +0530

    Init commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
-const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
 var app = express();
+
 app.use(express.static(publicPath));
 
 app.listen(port,()=>{
